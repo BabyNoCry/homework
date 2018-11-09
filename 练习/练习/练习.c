7df03c9237e119c05351f8eabdb189e593ade238
BabyNoCry <1633007250@qq.com>
2018-11-09 23:45:39 +0800

    理解练习

---
+++ 练习/练习/练习.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5 };
+//	int i = sizeof(&arr);//地址的大小
+//	printf("%d",i);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	char a[5] = {'a','b','c','d','e'};
+//	char(*ptr1)[10] = &a;
+//	char(*ptr2)[10] = a;
+//	printf("%p\n%p", ptr1, ptr1 + 1);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%d\n",&p[4][2]);//17825188
+//	printf("%d\n", &a[4][2]);//17825204
+//	printf("%d", &p[4][2] - &a[4][2]);//-4   虽然地址相差16个字节  p指针每次移动4个字节 因此相减结果位-4；
+//	system("pause");
+//	return 0;
+//}
+
